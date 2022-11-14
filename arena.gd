58d66b1c69da5b5c47b20f8185d3abc1687e3c5a
DetroFitzEmmy <yogibaconbiscuit@hotmail.com>
2022-11-14 16:41:57 -0500

    Fixed spawning and made map bigger

--- arena.gd
+++ arena.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
 var rng = RandomNumberGenerator.new()
+var camera2d
+var instance
 var skeletons = preload("res://enemies/enemy.tscn")
+const x = 400
+const y = 400
+
 #var spawn_position = get_node("Player").position + Vector2(100, 0).rotated(rng.randf_range(0, 2*PI))
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rng.randomize()
+	camera2d = get_node("Player/Camera2d")
 	print(get_node("Player").position)
+	print(get_node("Player").get_child(1))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +22,23 @@ func _process(delta):
 	pass
 
 
+
 func _on_enemy_spawn_timer_timeout():
-	var instance = skeletons.instantiate()
+	instance = skeletons.instantiate()
 	add_child(instance)
+	var center = camera2d.get_target_position()
+	instance.position = center
+	var direction = rng.randi_range(0,3)
+	if direction == 0: # up
+		instance.position.x += rng.randf_range(-x, x)
+		instance.position.y -= y
+	elif direction == 1: # down
+		instance.position.x += rng.randf_range(-x, x)
+		instance.position.y += y
+	elif direction == 2: # left
+		instance.position.x -= x
+		instance.position.y += rng.randf_range(-y, y)
+	elif direction == 3: # right
+		instance.position.x += x
+		instance.position.y += rng.randf_range(-y, y)
+	
--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -5,21 +5,15 @@ var rng = RandomNumberGenerator.new()
 var spawn_position
 
 func _ready():
-	rng.randomize()
-	if(spawn_position == null):
-		print("hello")
-	else:
-		print(spawn_position)
+	pass
 
 
 func _physics_process(delta):
 	var player = get_parent().get_node("Player")
 	position += (player.position - position).normalized() * 0.7
+	
 
 
 
-func randomizeSpawn():
-	print(spawn_position)
-	position = spawn_position
 	
 	

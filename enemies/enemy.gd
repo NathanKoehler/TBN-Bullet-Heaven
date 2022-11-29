9d93fda45899479fa6a0a88cd14ab950d7cf0ac7
DetroFitzEmmy <yogibaconbiscuit@hotmail.com>
2022-11-29 15:40:24 -0500

    Added hitboxes and hp damage to player

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,7 +1,14 @@
 extends CharacterBody2D
 
-var rng = RandomNumberGenerator.new()
 
+@export var hp = 10
+@export var hp_max = 10
+@export var defense = 0
+@export var level = 0
+@export var speed = .7
+
+
+var rng = RandomNumberGenerator.new()
 var spawn_position
 
 func _ready():
@@ -10,10 +17,10 @@ func _ready():
 
 func _physics_process(delta):
 	var player = get_parent().get_node("Player")
-	position += (player.position - position).normalized() * 0.7
-	
+	position += (player.position - position).normalized() * speed
 
 
-
-	
+func _on_hurtbox_area_entered(hitbox):
+	var base_damage = hitbox.damage
+	self.hp -= base_damage
 	
--- player/CharacterBody2d.gd
+++ player/CharacterBody2d.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
 
-var speed = 100
+@export var speed = 100
 @export var currPosition = position
+@export var hp_max = 100
+@export var hp = 100
+@export var defense = 5
+var level = 0
+var xp = 0
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -30,3 +36,14 @@ func handle_input():
 
 func get_position():
 	return position
+
+
+func receive_damage(base_damage):
+	var actual_damage = base_damage
+	actual_damage -= defense
+	hp -= actual_damage
+	print(hp)
+	print(str(name) + " received " + str(actual_damage) + " damage")
+
+func _on_hurtbox_area_entered(hitbox):
+	receive_damage(hitbox.damage)

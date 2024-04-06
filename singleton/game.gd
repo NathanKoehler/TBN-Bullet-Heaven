097f9f7920fa622dece474b2711c5d62942f29d6
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2024-04-06 16:45:58 -0400

    Made 2 UI sections for both players

--- singleton/game.gd
+++ singleton/game.gd
@@ -4,18 +4,20 @@ extends Node
 	"1": {
 		viewport = $Control/HBoxContainer/SubViewportContainer/SubViewport,
 		camera = $Control/HBoxContainer/SubViewportContainer/SubViewport/Camera2D,
-		player = $Control/HBoxContainer/SubViewportContainer/SubViewport/Arena/Player1
+		player = $Control/HBoxContainer/SubViewportContainer/SubViewport/Arena/Player1,
+		playerLevelText = $Control/HUDMargin/P1Level/P1LevelText,
+		playerLevelXPBar = $Control/HUDMargin/P1Level/P1LevelXPBar,
 	},
 	"2": {
 		viewport = $Control/HBoxContainer/SubViewportContainer2/SubViewport,
 		camera = $Control/HBoxContainer/SubViewportContainer2/SubViewport/Camera2D,
-		player = $Control/HBoxContainer/SubViewportContainer/SubViewport/Arena/Player2
+		player = $Control/HBoxContainer/SubViewportContainer/SubViewport/Arena/Player2,
+		playerLevelText = $Control/HUDMargin/P2Level/P2LevelText,
+		playerLevelXPBar = $Control/HUDMargin/P2Level/P2LevelXPBar,
 	}
 }
 
 func _ready() -> void:
-	print(players["2"].viewport)
-	print(players["1"].viewport)
 	players["2"].viewport.get_viewport().world_2d = players["1"].viewport.get_viewport().world_2d
 	for node in players.values():
 		var remote_transform := RemoteTransform2D.new()

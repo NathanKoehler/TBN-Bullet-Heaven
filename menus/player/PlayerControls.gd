a243ab924c33daa698be0200fd5de2a3fcc1dda7
Lin, Matthew <mlin306@gatech.edu>
2024-04-22 18:01:58 -0400

    Full controller support added

--- menus/player/PlayerControls.gd
+++ menus/player/PlayerControls.gd
@@ -1,8 +1,15 @@
 class_name PlayerControls
 extends Resource
 
+#movement
 @export var player_index := 0
 @export var move_left := "p1_move_left"
 @export var move_right := "p1_move_right"
 @export var move_up := "p1_move_up"
 @export var move_down := "p1_move_down"
+
+#shooting
+@export var aim_left := "p1_aim_left"
+@export var aim_right := "p1_aim_right"
+@export var aim_up := "p1_aim_up"
+@export var aim_down := "p1_aim_down"
